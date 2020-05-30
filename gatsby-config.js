36f41336968514ae59978825e709350210b9d075
FunnyGitHub <57555783+FunnyGitHub@users.noreply.github.com>
2020-05-30 11:19:40 +0200

    Ändere Titel

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Fanny tries to understand the world`,
     author: {
       name: `Kyle Mathews`,
       summary: `who lives and works in San Francisco building useful things.`
